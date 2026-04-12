56b7a489b5deb1f98a5baa33872550cbe1f0840f
Nishaad <72539638+1darshanpatil@users.noreply.github.com>
2026-04-12 16:29:29 +0530

    Initial Commit

---
+++ stealthx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stealthx
+//
+//  Created by Nishaad on 12/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stealthx/stealthxApp.swift
@@ -0,0 +1,17 @@
+//
+//  stealthxApp.swift
+//  stealthx
+//
+//  Created by Nishaad on 12/04/26.
+//
+
+import SwiftUI
+
+@main
+struct stealthxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
